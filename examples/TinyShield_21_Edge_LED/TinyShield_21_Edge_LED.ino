df24cdde75f30b6163a28c15eb964d4200017363
info@tinycircuits.com <info@tinycircuits.com>
2019-01-04 11:21:38 -0500

    Update format

---
+++ examples/TinyShield_21_Edge_LED/TinyShield_21_Edge_LED.ino
@@ -0,0 +1,39 @@
+/*
+TinyDuino Edge LED Demo
+  
+April 9 2014, by Ben Rose
+
+This example code is in the public domain.
+
+http://www.tiny-circuits.com
+
+*/
+
+void setup()
+{
+  LedOn(0);//Pass a zero to turn all LEDs off
+}
+
+void loop()
+{
+  for(int i=1;i<=21;i++){
+    LedOn(i);
+    delay(20);
+  };
+}
+
+void LedOn(int ledNum)
+{
+  for(int i=4;i<10;i++){
+    pinMode(i, INPUT);
+    digitalWrite(i, LOW);
+  };
+  if(ledNum<1 || ledNum>21) return;
+  char highpin[21]={5,6,5,7,6,7,6,8,5,8,8,7,9,7,9,8,5,9,6,9,9};
+  char lowpin[21]= {6,5,7,5,7,6,8,6,8,5,7,8,7,9,8,9,9,5,9,6,4};
+  ledNum--;
+  digitalWrite(highpin[ledNum],HIGH);
+  digitalWrite(lowpin[ledNum],LOW);
+  pinMode(highpin[ledNum],OUTPUT);
+  pinMode(lowpin[ledNum],OUTPUT);
+}
